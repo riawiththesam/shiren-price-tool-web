fca3b2fa6a2c2e93dc97f3a9788f00591eacb821
riawiththesam <riawiththesam@gmail.com>
2024-03-02 20:55:00 +0900

    feat: 呪い、祝福に対応

--- src/features/item-search/hooks/use-item-search.ts
+++ src/features/item-search/hooks/use-item-search.ts
@@ -2,7 +2,38 @@ import { useState } from "react";
 import { list as makimonoMaster } from "./item-makimono.ts";
 import { list as udewaMaster } from "./item-udewa.ts";
 import { list as kusaMaster } from "./item-kusa.ts";
-import { Item } from "../types/Item.ts";
+import { Item, ItemState, MasterItem } from "../types/Item.ts";
+
+type ItemStateRateMapType = {
+  [key in ItemState]: number;
+};
+
+const itemStateRateMap: ItemStateRateMapType = {
+  Normal: 1,
+  Noroi: 0.87,
+  Shukufuku: 2,
+};
+
+function findItemList(
+  masterList: Array<MasterItem>,
+  price: number
+): Array<Item> {
+  const noroiList: Array<Item> = masterList
+    .filter((item) => Math.floor(item.buy * itemStateRateMap["Noroi"]) == price)
+    .map((item) => ({ ...item, state: "Noroi" }));
+  const shukufukuList: Array<Item> = masterList
+    .filter(
+      (item) => Math.floor(item.buy * itemStateRateMap["Shukufuku"]) == price
+    )
+    .map((item) => ({ ...item, state: "Shukufuku" }));
+  const normalList: Array<Item> = masterList
+    .filter(
+      (item) => Math.floor(item.buy * itemStateRateMap["Normal"]) == price
+    )
+    .map((item) => ({ ...item, state: "Normal" }));
+
+  return [...noroiList, ...shukufukuList, ...normalList];
+}
 
 export const useItemSearch = () => {
   const [makimonoList, setMakimonoList] = useState<Array<Item>>([]);
@@ -10,20 +41,15 @@ export const useItemSearch = () => {
   const [kusaList, setKusaList] = useState<Array<Item>>([]);
 
   const setPrice = (value: string) => {
-    const numberValue = Number.parseInt(value, 10);
-    const nextMakimonoList = makimonoMaster.list.filter((item) => {
-      return item.buy == numberValue;
-    });
+    const price = Number.parseInt(value, 10);
+
+    const nextMakimonoList = findItemList(makimonoMaster.list, price);
     setMakimonoList(nextMakimonoList);
 
-    const nextUdewaList = udewaMaster.list.filter(
-      (item) => item.buy == numberValue
-    );
+    const nextUdewaList = findItemList(udewaMaster.list, price);
     setUdewaList(nextUdewaList);
 
-    const nextKusaList = kusaMaster.list.filter(
-      (item) => item.buy == numberValue
-    );
+    const nextKusaList = findItemList(kusaMaster.list, price);
     setKusaList(nextKusaList);
   };
   return { makimonoList, udewaList, kusaList, setPrice };
--- src/features/item-search/types/Item.ts
+++ src/features/item-search/types/Item.ts
@@ -1,4 +1,14 @@
+export const ItemStateList = ["Normal", "Noroi", "Shukufuku"] as const;
+
+export type ItemState = (typeof ItemStateList)[number];
+
 export interface Item {
+  name: string;
+  buy: number;
+  state: ItemState;
+}
+
+export interface MasterItem {
   name: string;
   buy: number;
 }
