c31533ad28cf2297a79d3a415207c396110add11
riawiththesam <riawiththesam@gmail.com>
2024-03-16 01:17:01 +0900

    feat: CSS調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import "./App.css";
-import Box from "@mui/material/Box";
 import { ShirenAppBar } from "./features/shiren-app-bar/components/shiren-app-bar.tsx";
 import { ShirenDrawer } from "./features/shiren-app-bar/components/shiren-drawer.tsx";
 import { MainUI } from "./features/main-ui/components/main-ui.tsx";
 import { useMainUI } from "./features/main-ui/hooks/use-main-ui.ts";
+import { Box, Stack } from "@mui/material";
 
 function App() {
   const { mainUIState, toggleDrawer } = useMainUI();
 
   return (
     <>
-      <Box>
+      <Stack sx={{ height: "100%" }}>
         <ShirenAppBar onClickDrawerOpen={toggleDrawer} />
         <ShirenDrawer
           opened={mainUIState.drawerOpened}
           onClickClose={toggleDrawer}
         />
-        <MainUI />
-      </Box>
+        <Box sx={{ flexGrow: 1, overflowY: "scroll" }}>
+          <MainUI />
+        </Box>
+      </Stack>
     </>
   );
 }
--- src/features/item-list/components/item-list.tsx
+++ src/features/item-list/components/item-list.tsx
@@ -1,8 +1,7 @@
-import { Box, List } from "@mui/material";
+import { Box, Checkbox, List, Stack } from "@mui/material";
 import { FC } from "react";
 import { ItemWithPurchseType, useItemList } from "../hooks/use-item-list";
 import { CollapsibleListItem } from "./collapsible-list-item";
-import { ItemListSubheader } from "./item-list-subleader";
 
 function filterItemList(
   itemList: Array<ItemWithPurchseType>,
@@ -34,35 +33,42 @@ export const ItemList: FC = () => {
   } = useItemList();
 
   return (
-    <Box sx={{ margin: 5, maxWidth: 800 }}>
-      <List
-        sx={{ width: "100%" }}
-        subheader={
-          <ItemListSubheader
-            showBuyItems={showBuyItems}
-            toggleShowBuyItems={toggleShowBuyItems}
-            showSellItems={showSellItems}
-            toggleShowSellItems={toggleShowSellItems}
-          />
-        }
+    <Box sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+      <Stack
+        direction="row"
+        spacing={3}
+        sx={{ paddingX: "40px", alignItems: "center" }}
       >
-        {itemListState.groupedList.map((group) => {
-          const itemList = filterItemList(
-            group.itemList,
-            showBuyItems,
-            showSellItems
-          );
-          return itemList.length > 0 ? (
-            <CollapsibleListItem
-              key={group.value}
-              itemList={group.itemList}
-              listOpened={group.opened}
-              value={group.value}
-              onClick={toggleItemOpened}
-            />
-          ) : null;
-        })}
-      </List>
+        <Box>金額</Box>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          購入
+          <Checkbox checked={showBuyItems} onClick={toggleShowBuyItems} />
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          売却
+          <Checkbox checked={showSellItems} onClick={toggleShowSellItems} />
+        </Box>
+      </Stack>
+      <Box sx={{ flexGrow: 1, overflowY: "auto" }}>
+        <List>
+          {itemListState.groupedList.map((group) => {
+            const itemList = filterItemList(
+              group.itemList,
+              showBuyItems,
+              showSellItems
+            );
+            return itemList.length > 0 ? (
+              <CollapsibleListItem
+                key={group.value}
+                itemList={itemList}
+                listOpened={group.opened}
+                value={group.value}
+                onClick={toggleItemOpened}
+              />
+            ) : null;
+          })}
+        </List>
+      </Box>
     </Box>
   );
 };
