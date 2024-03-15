ecc892a2d0eb47f253664db94adbf8aef7031bd5
riawiththesam <riawiththesam@gmail.com>
2024-03-15 20:57:30 +0900

    feat: ソート

--- src/features/item-list/hooks/use-item-list.ts
+++ src/features/item-list/hooks/use-item-list.ts
@@ -4,6 +4,9 @@ import { Item } from "../../../types/Item";
 export const useItemList = () => {
   const rawItemList: Array<Item> = [];
   rawItemList.push(...getAllKusaList());
+  rawItemList.sort((a, b) => {
+    return a.buy - b.buy;
+  });
 
   return {
     rawItemList,
