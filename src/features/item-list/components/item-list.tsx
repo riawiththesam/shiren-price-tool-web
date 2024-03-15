25849933441a9928649219b9a8d861af84758b12
riawiththesam <riawiththesam@gmail.com>
2024-03-16 00:28:50 +0900

    feat: フィルタ処理を追加

--- src/features/item-list/components/item-list.tsx
+++ src/features/item-list/components/item-list.tsx
@@ -1,8 +1,27 @@
 import { Box, List } from "@mui/material";
 import { FC } from "react";
-import { useItemList } from "../hooks/use-item-list";
+import { ItemWithPurchseType, useItemList } from "../hooks/use-item-list";
 import { CollapsibleListItem } from "./collapsible-list-item";
 import { ItemListSubheader } from "./item-list-subleader";
+
+function filterItemList(
+  itemList: Array<ItemWithPurchseType>,
+  showBuyItems: boolean,
+  showSellItems: boolean
+): Array<ItemWithPurchseType> {
+  let filteredList = itemList;
+
+  // 購入アイテムを表示しない設定のときはpurchseTypeがbuyのものを除く
+  if (!showBuyItems) {
+    filteredList = filteredList.filter((pair) => pair.purchaseType != "buy");
+  }
+  // 売却アイテムを表示しない設定のときはpurchseTypeがsellのものを除く
+  if (!showSellItems) {
+    filteredList = filteredList.filter((pair) => pair.purchaseType != "sell");
+  }
+
+  return filteredList;
+}
 
 export const ItemList: FC = () => {
   const {
@@ -28,7 +47,12 @@ export const ItemList: FC = () => {
         }
       >
         {itemListState.groupedList.map((group) => {
-          return (
+          const itemList = filterItemList(
+            group.itemList,
+            showBuyItems,
+            showSellItems
+          );
+          return itemList.length > 0 ? (
             <CollapsibleListItem
               key={group.value}
               itemList={group.itemList}
@@ -36,7 +60,7 @@ export const ItemList: FC = () => {
               value={group.value}
               onClick={toggleItemOpened}
             />
-          );
+          ) : null;
         })}
       </List>
     </Box>
