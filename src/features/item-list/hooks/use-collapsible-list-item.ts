a6f30151596a0c4b83d6f3a950cddac701c218d7
riawiththesam <riawiththesam@gmail.com>
2024-09-19 02:34:04 +0900

    doc: ドキュメンテーションコメント追加

--- src/features/item-list/hooks/use-collapsible-list-item.ts
+++ src/features/item-list/hooks/use-collapsible-list-item.ts
@@ -9,12 +9,21 @@ export interface CollapsibleListItemState {
   tableOpened: boolean;
 }
 
+/**
+ * CollapsibleListItemのステート管理
+ * 表の表示状態、ダイアログの内容、表示状態
+ *
+ * @param itemGroup
+ * @returns
+ */
 export function useCollapsibleListItem(itemGroup: ItemGroup) {
   const [state, setState] = useImmer<CollapsibleListItemState>({
     isOpened: false,
     item: null,
     tableOpened: false,
   });
+  // itemGroupが更新されたら表を閉じる
+  // itemGroupを受け取るよりきれいなやり方がありそう
   useEffect(() => {
     setState((draft) => {
       draft.tableOpened = false;
@@ -23,18 +32,29 @@ export function useCollapsibleListItem(itemGroup: ItemGroup) {
 
   return {
     state,
+    /**
+     * ダイアログを表示する
+     * @param item
+     */
     setOpened: (item: Item) => {
       setState((draft) => {
         draft.item = item;
         draft.isOpened = true;
       });
     },
+    /**
+     * ダイアログを閉じる
+     */
     close: () => {
       setState((draft) => {
         draft.item = null;
         draft.isOpened = false;
       });
     },
+    /**
+     * テーブルを表示する
+     * @param opened
+     */
     setTableOpened: (opened: boolean) => {
       setState((draft) => {
         draft.tableOpened = opened;
