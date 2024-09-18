256c575f2f6f7292b099db38883dc60f778bccec
riawiththesam <riawiththesam@gmail.com>
2024-09-19 03:40:35 +0900

    test: useMainUIのドロワー表示状態のテスト

--- src/features/main-ui/hooks/use-main-ui.spec.ts
+++ src/features/main-ui/hooks/use-main-ui.spec.ts
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { MainUIState, useMainUI } from "./use-main-ui";
 
 describe("use-main-ui", () => {
@@ -14,5 +14,23 @@ describe("use-main-ui", () => {
     const { result } = renderHook(() => useMainUI());
 
     expect(result.current.mainUIState).toEqual(expected);
+  });
+
+  it("useMainUIのドロワーを表示", () => {
+    const { result } = renderHook(() => useMainUI());
+    act(() => {
+      result.current.toggleDrawer();
+    });
+    expect(result.current.mainUIState).toEqual({
+      featureUIState: "ItemList",
+      drawerOpened: true,
+    });
+    act(() => {
+      result.current.toggleDrawer();
+    });
+    expect(result.current.mainUIState).toEqual({
+      featureUIState: "ItemList",
+      drawerOpened: false,
+    });
   });
 });
