1a5b92218140182c85b610cc1c24a54c817a376c
riawiththesam <riawiththesam@gmail.com>
2024-09-19 03:02:46 +0900

    test: setPurchaseTypeのtest

--- src/features/item-list/hooks/use-item-list.spec.ts
+++ src/features/item-list/hooks/use-item-list.spec.ts
@@ -16,4 +16,47 @@ describe("use-item-list", () => {
 
     expect(result.current.itemListState.filter).toEqual(expectedFilter);
   });
+
+  it("useItemListのpurchaseTypeをbuyに設定", () => {
+    const expectedFilter: ItemFilter = {
+      purchaseType: "buy",
+      showItemType: "All",
+      enableUnique: false,
+    };
+    const { result } = renderHook(() => useItemList());
+    act(() => {
+      result.current.setPurchaseType("buy");
+    });
+
+    expect(result.current.itemListState.filter).toEqual(expectedFilter);
+  });
+
+  it("useItemListのpurchaseTypeをsellに設定", () => {
+    const expectedFilter: ItemFilter = {
+      purchaseType: "sell",
+      showItemType: "All",
+      enableUnique: false,
+    };
+    const { result } = renderHook(() => useItemList());
+    act(() => {
+      result.current.setPurchaseType("sell");
+    });
+
+    expect(result.current.itemListState.filter).toEqual(expectedFilter);
+  });
+
+  it("useItemListのpurchaseTypeをallに設定", () => {
+    const expectedFilter: ItemFilter = {
+      purchaseType: "all",
+      showItemType: "All",
+      enableUnique: false,
+    };
+    const { result } = renderHook(() => useItemList());
+    act(() => {
+      result.current.setPurchaseType("sell");
+      result.current.setPurchaseType("all");
+    });
+
+    expect(result.current.itemListState.filter).toEqual(expectedFilter);
+  });
 });
